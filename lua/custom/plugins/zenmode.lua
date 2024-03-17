b15dc730d885301d09a34a6e8a571c8af114ba83
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2024-03-17 12:51:20 +0100

    sync

--- lua/custom/plugins/zenmode.lua
+++ lua/custom/plugins/zenmode.lua
@@ -1,4 +1,21 @@
 return {
   'folke/zen-mode.nvim',
-  config = true,
+  dependencies = {
+    'folke/twilight.nvim',
+  },
+  opts = {
+    window = {
+      options = {
+        number = false,
+        relativenumber = false,
+      },
+    },
+
+    plugins = {
+      alacritty = {
+        enabled = true,
+        font = '16',
+      },
+    },
+  },
 }
